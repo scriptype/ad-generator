afae9aee93a6ccce7090d7e1c90c385ece37adc3
scriptype <scriptyper@gmail.com>
2019-03-22 00:35:21 +0200

    Create generator/utils and add truncate function

---
+++ generator/utils.js
@@ -0,0 +1,12 @@
+function truncate (text, limit, withEllipsis) {
+  if (limit && limit >= text.length) {
+    return text
+  }
+  const truncatedText = text.slice(0, limit)
+  const ellipsis = withEllipsis ? '...' : ''
+  return truncatedText + ellipsis
+}
+
+module.exports = {
+  truncate
+}
