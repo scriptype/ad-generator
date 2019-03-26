1de9ab2e54284ec1b47867d131596a435198740e
scriptype <scriptyper@gmail.com>
2019-03-26 21:00:44 +0200

    Fix livereload.js path to enable livereloading in mobile

--- scripts/tasks/handlebars-index-dev.js
+++ scripts/tasks/handlebars-index-dev.js
@@ -13,7 +13,16 @@ const {
 } = process.env
 
 const js = [
-  '<script src="http://localhost:35729/livereload.js?snipver=1"></script>'
+  `
+  <script>
+    document.write(
+      '<script src="http://' +
+      (location.host || 'localhost').split(':')[0] +
+      ':35729/livereload.js?snipver=1"></' +
+      'script>'
+    )
+  </script>
+  `
 ]
 
 if (fileExists(JS_OUTPUT)) {
