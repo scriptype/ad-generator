ccfb1ea97addb9cd02d331935f3ca9e7748b3e8d
scriptype <scriptyper@gmail.com>
2019-03-31 12:47:41 +0300

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   "extends": "standard",
   "parser": "babel-eslint",
-  "env": ["browser"],
+  "env": {
+    "browser": true
+  },
   "rules": {
     "space-before-function-paren": ["error", {
       "anonymous": "never",
