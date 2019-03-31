233a9addc3068be04c3f23f218344c6af21c4495
scriptype <scriptyper@gmail.com>
2019-03-31 21:35:48 +0300

    Fix generator test
    - It was not passing type parameter. Now there are two test cases for two types (single, carousel)

--- dist/app.js
+++
@@ -1,3 +0,0 @@
-import AdCarousel from './app/ad-carousel.js'
-
-AdCarousel.init()
--- generator/index.spec.js
+++ generator/index.spec.js
@@ -4,21 +4,39 @@ const { fileExists } = require('../lib/utils')
 const { HTML_OUTPUT_MIN } = require('../scripts/env')
 const testAppData = require('../test-app-itunes.json')
 
-test.cb('Generator generates HTML', t => {
-  const ps = exec(`node generator ${testAppData.trackId}`)
+function generate(type, id) {
+  return new Promise((resolve, reject) => {
+    const ps = exec(`node generator ${type} ${id}`)
 
-  ps.stderr.pipe(process.stderr)
+    ps.stderr.pipe(process.stderr)
 
-  ps.on('close', code => {
-    if (code === 0) {
-      t.true(
-        fileExists(HTML_OUTPUT_MIN),
-        'HTML output is generated'
-      )
-      t.end()
-    } else {
-      t.fail('exited with non-zero')
-      t.end()
-    }
+    ps.on('close', code => {
+      if (code === 0) resolve()
+      else reject(code)
+    })
   })
+}
+
+test.serial('Generator generates single ad', async (t) => {
+  try {
+    await generate('single', testAppData.trackId)
+  } catch (e) {
+    t.fail('exited with non-zero')
+  }
+  t.true(
+    fileExists(HTML_OUTPUT_MIN),
+    'HTML output is generated'
+  )
+})
+
+test.serial('Generator generates carousel ad', async (t) => {
+  try {
+    await generate('carousel', testAppData.trackId)
+  } catch (e) {
+    t.fail('exited with non-zero')
+  }
+  t.true(
+    fileExists(HTML_OUTPUT_MIN),
+    'HTML output is generated'
+  )
 })
