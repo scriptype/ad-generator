ffd84cc685ca0db914df1b176c7c747be53e7903
scriptype <scriptyper@gmail.com>
2019-03-31 19:07:51 +0300

    Add tests for utils.addClass

--- dist/app/utils.spec.js
+++ dist/app/utils.spec.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import { debounce } from './utils.mjs'
+import { debounce, addClass } from './utils.mjs'
 
 test.cb('debounce', t => {
   t.plan(3)
@@ -27,4 +27,27 @@ test.cb('debounce', t => {
       t.end()
     }
   }, 50)
+})
+
+test('addClass', t => {
+  const myElement = document.createElement('div')
+  const testClass1 = 'my-test-class'
+  const testClass2 = 'my-test-class-2'
+  const testClass3 = ''
+  const testClass4 = null
+  const testClass5 = undefined
+
+  addClass(myElement, testClass1)
+  t.true(
+    myElement.classList.contains(testClass1),
+    'It adds the className'
+  )
+
+  addClass(myElement, testClass2, testClass3, testClass4, testClass5)
+  t.pass('It does not throw error if some of the given classes are falsy')
+  t.deepEqual(
+    [...myElement.classList.values()],
+    [ testClass1, testClass2 ],
+    'It only adds thruthy valid classnames'
+  )
 })
