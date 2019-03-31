3990410d48e257a8dfb9b4382e25e29d8a108b39
scriptype <scriptyper@gmail.com>
2019-03-31 19:46:58 +0300

    Add tests for showIndicators param of Carousel

--- src/app/lib/carousel.spec.js
+++ src/app/lib/carousel.spec.js
@@ -1,13 +1,18 @@
 import test from 'ava'
 import carousel from './carousel'
 
-function createEl() {
+function getImageUrl(index) {
+  return `<img src="http://lorempixel.com/800/800/sports/${index}" />`
+}
+
+function nArray(n, mapFn) {
+  return [...Array(n)].map(mapFn)
+}
+
+function createEl(imageCount) {
   const markup = `
     <div id="my-el">
-      <img src="http://lorempixel.com/800/800/sports/5" />
-      <img src="http://lorempixel.com/800/800/sports/4" />
-      <img src="http://lorempixel.com/800/800/sports/3" />
-      <img src="http://lorempixel.com/800/800/sports/2" />
+      ${nArray(imageCount, (item, index) => getImageUrl(index)).join('')}
     </div>
   `
   const el = document.createElement('div')
@@ -16,10 +21,32 @@ function createEl() {
 }
 
 test('Basic parameters', t => {
-  const myContainer = createEl()
+  const myContainer = createEl(3)
   carousel({
     el: myContainer
   })
 
   t.pass('It runs when no parameter other than el is passed')
+})
+
+test('Indicators', t => {
+  const imageCount = 5
+  const myContainer = createEl(imageCount)
+  const myCarousel = carousel({
+    el: myContainer,
+    showIndicators: true
+  })
+
+  const { defaultClassNames } = myCarousel.__test__
+  t.is(
+    myContainer.querySelectorAll(`.${defaultClassNames.INDICATOR}`).length,
+    imageCount,
+    'Correct amount of indicators created'
+  )
+
+  t.is(
+    myContainer.querySelectorAll(`.${defaultClassNames.INDICATOR_ACTIVE}`).length,
+    1,
+    'There is only one active indicator'
+  )
 })
