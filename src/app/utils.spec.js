da77ca9c7d8d4bc77c76713b0882cccf79f6bb40
scriptype <scriptyper@gmail.com>
2019-03-31 18:49:14 +0300

    Add new test case for utils.debounce

--- src/app/utils.spec.js
+++ src/app/utils.spec.js
@@ -2,18 +2,22 @@ import test from 'ava'
 import { debounce } from './utils.mjs'
 
 test.cb('debounce', t => {
-  t.plan(1)
+  t.plan(3)
 
-  function testFunction() {
+  function testFunction(arg1, arg2) {
+    t.is(arg1, testArg1, 'It passes arguments to debounced function')
+    t.is(arg2, testArg2, 'It passes multiple arguments to debounced function')
     runTimes++
   }
 
-  const debouncedFunction = debounce(testFunction, 300)
-
   let runTimes = 0
   let intervalLimit = 6
+  const testArg1 = 'my argument'
+  const testArg2 = 'my second argument'
+  const debouncedFunction = debounce(testFunction, 300)
+
   const interval = setInterval(() => {
-    debouncedFunction()
+    debouncedFunction(testArg1, testArg2)
     if (intervalLimit-- === 0) {
       clearInterval(interval)
       t.is(
